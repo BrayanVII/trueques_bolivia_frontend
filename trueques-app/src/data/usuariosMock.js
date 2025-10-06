6f4ce60ada65f0c6c4b90e1ef97e5463cc847726
BrayanVII <programacionavanzadaiii@gmail.com>
2025-10-05 23:41:57 -0400

    feat: Agregar Home funcional

---
+++ trueques-app/src/data/usuariosMock.js
@@ -0,0 +1,4 @@
+export const usuariosMock = [
+  { id: 1, nombre: 'Brayan', correo: 'brayan@gmail.com', rol: 'Administrador' },
+  { id: 2, nombre: 'Fernando', correo: 'fernando@gmail.com', rol: 'Usuario' },
+];
