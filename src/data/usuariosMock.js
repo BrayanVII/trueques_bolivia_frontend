7e0012e2f339c615623e0be3d287aeadb9666310
BrayanVII <programacionavanzadaiii@gmail.com>
2025-11-23 11:46:23 -0400

    Borrar todo el contenido

--- src/data/usuariosMock.js
+++
@@ -1,4 +0,0 @@
-export const usuariosMock = [
-  { id: 1, nombre: 'Brayan', correo: 'brayan@gmail.com', rol: 'Administrador' },
-  { id: 2, nombre: 'Fernando', correo: 'fernando@gmail.com', rol: 'Usuario' },
-];
