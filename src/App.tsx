af072246f8886ebdc938f9dde374aba21be96d05
BrayanVII <programacionavanzadaiii@gmail.com>
2025-12-01 19:35:09 -0400

    feat:Actualice App.tsx con nuevas funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Home } from "./pages/Home";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import UsuarioPage from "./pages/UsuarioPage";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/usuario" element={<UsuarioPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
